080321c5b44b1e617a879c3ddf5ebbe61bf655b4
djole103 <djordje_7@hotmail.com>
2015-05-26 13:11:49 -0400

    v3 security unecessary for basic monitoring, authentication/user creation too much work

--- snmp.sh
+++ snmp.sh
@@ -1,34 +1,36 @@
 #! /bin/bash
 while [ True ]; do
-	#snmpwalk localhost UCD-SNMP-MIB::systemStats |	logger -p local0.INFO 
-	
-	#available ram  
-	ramfree=$(snmpget localhost .1.3.6.1.4.1.2021.4.11.0)
-	IFS=', ' read -a ramarray <<< "$ramfree"
-	if [ ${ramarray[3]} -lt 100000 ]; then
-		logger -p local0.WARN $ramfree 
-	else
-		logger -p local0.INFO $ramfree
-	fi
-	
-	#available disc
-	discfree=$(snmpget localhost .1.3.6.1.4.1.2021.9.1.7.1)
-	IFS=', ' read -a discarray <<< "$discfree"
-	if [ ${discarray[3]} -lt 100000 ]; then
-		logger -p local0.WARN $discfree
-	else
-		logger -p local0.INFO $discfree
-	fi
-	
-	#1min avg cpu load
-	avgload=$(snmpget localhost .1.3.6.1.4.1.2021.10.1.3.1)	
-	IFS=', ' read -a loadarray <<< "$avgload"	
-	if [ $(bc <<< "${loadarray[3]} > 8") ]; then
 
-		logger -p local0.WARN $avgload
-	else
-		logger -p local0.INFO $avgload
-	fi
+        ip=$(ip route get 8.8.8.8 | awk 'NR==1 {print $NF}')
 
-	sleep 5
+        #available ram  
+        ramfree=$(snmpget -v2c -c public localhost .1.3.6.1.4.1.2021.4.11.0)
+        IFS=', ' read -a ramarray <<< "$ramfree"
+        if [ ${ramarray[3]} -lt 100000 ]; then
+                logger -p local0.WARN $ip  RAM available: ${ramarray[3]} #$ramfree 
+        else
+                logger -p local0.INFO $ip  RAM available: ${ramarray[3]}  #$ramfree
+        fi
+
+        #available disc
+        discfree=$(snmpget -v2c -c public localhost .1.3.6.1.4.1.2021.9.1.7.1)
+        IFS=', ' read -a discarray <<< "$discfree"
+        if [ ${discarray[3]} -lt 100000 ]; then
+                logger -p local0.WARN $ip DISC available: ${discarray[3]} #$discfree
+        else
+                logger -p local0.INFO $ip DISC available: ${discarray[3]} #$discfree
+        fi
+
+        #1min avg cpu load
+        avgload=$(snmpget -v2c -c public localhost .1.3.6.1.4.1.2021.10.1.3.1)
+        IFS=', ' read -a loadarray <<< "$avgload"
+        if [ $(bc <<< "${loadarray[3]} > 8") ]; then
+
+                logger -p local0.WARN $ip CPU average load for 1 minute: ${loadarray[3]} #$avgload
+        else
+                logger -p local0.INFO $ip CPU average load for 1 minute: ${loadarray[3]} # $avgload
+        fi
+
+        sleep 5
 done
+
